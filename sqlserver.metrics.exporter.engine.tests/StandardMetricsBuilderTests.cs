c3c1cadda141faf6b57604cea4ae983fdb78acc8
DEV\aklein <Andreas.Kleinbichler@admiral.at>
2021-12-18 19:21:40 +0100

    Refactor, generate tests for min/max  elapsed time metrics.

--- sqlserver.metrics.exporter.engine.tests/StandardMetricsBuilderTests.cs
+++ sqlserver.metrics.exporter.engine.tests/StandardMetricsBuilderTests.cs
@@ -1,5 +1,7 @@
-﻿using NUnit.Framework;
+﻿using FluentAssertions;
+using NUnit.Framework;
 using SqlServer.Metrics.Provider;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -9,7 +11,7 @@ namespace Sqlserver.Metrics.Provider.Tests
     public class StandardMetricsBuilderTests
     {
         [Test]
-        public void Build_FiguresForElapsedTimeSupplied_ReturnsElapsedMetricItems()
+        public void Build_SingleFiguresForElapsedTimeSupplied_ReturnsElapsedMetricItems()
         {
             string storedProcedureName = "MySp";
             int maxElapsedTime = 150;
@@ -43,6 +45,68 @@ namespace Sqlserver.Metrics.Provider.Tests
             StandardMetricsBuilder instanceUnderTest = new StandardMetricsBuilder();
 
             var result = instanceUnderTest.Build(groupedPlanCacheItems);
+        }
+
+        [Test]
+        public void Build_MultipleFiguresForElapsedTimeSupplied_ReturnsElapsedMetricItems()
+        {
+            string storedProcedureName = "MySp";
+            int maxElapsedTime = 150;
+            int minElapsedTime = 10;
+            int betweenMaxElapsedTime = 70;
+            int betweenMinElapsedTime = 30;
+            DateTime removedFromCacheAt1 = DateTime.Parse("2021-12-12 17:34:04");
+            DateTime removedFormCacheAt2 = DateTime.Parse("2021-12-12 17:30:04");
+            List<MetricItem> expectedItems =
+              new List<MetricItem>()
+              {
+                    new MetricItem()
+                    {
+                        Name = $"{storedProcedureName}_ElapsedTimeMax",
+                        Value = maxElapsedTime
+                    },
+                    new MetricItem()
+                    {
+                        Name = $"{storedProcedureName}_ElapsedTimeMin",
+                        Value = minElapsedTime
+                    }
+              };
+            var groupedPlanCacheItems =
+                (new List<PlanCacheItem>() {
+                    new PlanCacheItem()
+                    {
+                        RemovedFromCacheAt = null,
+                        SpName = storedProcedureName,
+                        ExecutionStatistics = new ProcedureExecutionStatistics()
+                        {
+                            ElapsedTime = new ElapsedTime() { Max = maxElapsedTime, Min = minElapsedTime }
+                        }
+                    },
+                    new PlanCacheItem()
+                    {
+                        RemovedFromCacheAt = removedFromCacheAt1,
+                        SpName = storedProcedureName,
+                        ExecutionStatistics = new ProcedureExecutionStatistics()
+                        {
+                            ElapsedTime = new ElapsedTime() { Max = betweenMaxElapsedTime, Min = betweenMinElapsedTime }
+                        }
+                    },
+                    new PlanCacheItem()
+                    {
+                        RemovedFromCacheAt = removedFormCacheAt2,
+                        SpName = storedProcedureName,
+                        ExecutionStatistics = new ProcedureExecutionStatistics()
+                        {
+                            ElapsedTime = new ElapsedTime() { Max = betweenMaxElapsedTime, Min = betweenMinElapsedTime }
+                        }
+                    }
+                }).GroupBy(p => p.SpName).First();
+
+            StandardMetricsBuilder instanceUnderTest = new StandardMetricsBuilder();
+
+            var result = instanceUnderTest.Build(groupedPlanCacheItems);
+
+            result.Should().BeEquivalentTo(expectedItems);
         }
     }
 }
--- sqlserver.metrics.provider/StandardMetricsBuilder.cs
+++ sqlserver.metrics.provider/StandardMetricsBuilder.cs
@@ -1,5 +1,4 @@
 ﻿using SqlServer.Metrics.Provider;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
