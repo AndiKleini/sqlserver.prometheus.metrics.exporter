d27cc93e0d21d4339749ffb2b4e98d55e999078c
DEV\aklein <Andreas.Kleinbichler@admiral.at>
2022-02-18 06:35:21 +0100

    feature: Implement InMemoryLastFetchHistory.

---
+++ Sqlserver.Metrics.Exporter.Tests/Services/InMemoryLastFetchHistoryTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Sqlserver.Metrics.Exporter.Services;
+using System;
+
+namespace Sqlserver.Metrics.Exporter.Tests.Services
+{
+    [TestFixture]
+    public class InMemoryLastFetchHistoryTests
+    {
+        [Test]
+        public void GetPreviousFetchAndResetToNow_CalledForTheFirstTime_ReturnsNull()
+        {
+            Assert.IsNull(new InMemoryLastFetchHistory().GetPreviousFetchAndResetToNow());
+        }
+
+        [Test]
+        public void GetPreviousFetchAndResetToNow_CalledForTheSecondTime_ReturnsTimeStampOfPreviousCall()
+        {
+            var instanceUnderTest = new InMemoryLastFetchHistory();
+            DateTime timeBeforeFirstFetch = DateTime.Now;
+            
+            _ = instanceUnderTest.GetPreviousFetchAndResetToNow();
+            DateTime? resultOfSecondCall = instanceUnderTest.GetPreviousFetchAndResetToNow();
+
+            resultOfSecondCall.Should().BeAfter(timeBeforeFirstFetch);
+        }
+    }
+}
---
+++ sqlserver.metrics.exporter/Services/InMemoryLastFetchHistory.cs
@@ -0,0 +1,17 @@
+﻿using Sqlserver.Metrics.Exporter.Services;
+using System;
+
+namespace Sqlserver.Metrics.Exporter.Services
+{
+    public class InMemoryLastFetchHistory : ILastFetchHistory
+    {
+        private DateTime? callTimeStamp = null;
+
+        public DateTime? GetPreviousFetchAndResetToNow()
+        {
+            var previousTimeStamp = this.callTimeStamp;
+            this.callTimeStamp = DateTime.Now;
+            return previousTimeStamp;
+        }
+    }
+}
