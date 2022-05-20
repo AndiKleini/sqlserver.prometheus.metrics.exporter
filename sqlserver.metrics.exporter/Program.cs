4c107c54b1d475ecd253e7d308b4bc594f6f2c15
Andreas Kleinbichler <andreas.kleinbichler@admiral.at>
2022-05-20 07:30:49 +0200

    feature: load configuration from ./config folder.

--- sqlserver.metrics.exporter/Program.cs
+++ sqlserver.metrics.exporter/Program.cs
@@ -1,20 +1,28 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace SqlServer.metrics.exporter
 {
     public class Program
     {
+        private const string APPSETTINGS_PATH = "./config/appsettings.json";
+
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                    .ConfigureWebHostDefaults(webBuilder =>
+                    {
+                        webBuilder.UseStartup<Startup>();
+                    }).ConfigureAppConfiguration(b =>
+                    {
+                        b.AddJsonFile(APPSETTINGS_PATH, true, false);
+                    });
+        }
     }
 }
