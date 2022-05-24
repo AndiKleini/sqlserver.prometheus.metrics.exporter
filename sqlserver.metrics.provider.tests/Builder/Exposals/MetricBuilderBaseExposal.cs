d1f201a20bb089f54a757ff1d3f23d01790dc7c7
Andreas Kleinbichler <andreas.kleinbichler@admiral.at>
2022-05-24 21:46:31 +0200

    test: create tests for the naming generation.

---
+++ sqlserver.metrics.provider.tests/Builder/Exposals/MetricBuilderBaseExposal.cs
@@ -0,0 +1,9 @@
+﻿using SqlServer.Metrics.Provider.Builder;
+
+namespace SqlServer.Metrics.Provider.Tests.Builder.Exposals
+{
+    public class MetricBuilderBaseExposal : MetricsBuilderBase
+    {
+        internal new string GetMetricsName(string procedureName, string metricsName) => base.GetMetricsName(procedureName, metricsName);
+    }
+}
---
+++ sqlserver.metrics.provider.tests/Builder/MetricBuilderBaseTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using SqlServer.Metrics.Provider.Tests.Builder.Exposals;
+
+namespace SqlServer.Metrics.Provider.Tests.Builder
+{
+    [TestFixture]
+    public class MetricBuilderBaseTests
+    {
+        [Test]
+        public void GetMetricName_ProcMetricValueProvided_ReturnsMetricName()
+        {
+            const string ProcedureName = "myProc";
+            const string MetricsName = "myMetrics";
+            string metricsName = new MetricBuilderBaseExposal().GetMetricsName(ProcedureName, MetricsName);
+            metricsName.Should().Be($"MSSQL_{MetricsName}{{\"storedprocedure={ProcedureName}\"}}");
+        }
+    }
+}
